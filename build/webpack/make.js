ed3115f36c541658de3018d25e40ec8b0aa79031
David Zukowski <zukoweb@gmail.com>
2015-05-17 15:52:32 -0400

    refactor(build): moves merge utils to ~/build/utils

--- build/webpack/make.js
+++ build/webpack/make.js
@@ -1,5 +1,5 @@
 const env   = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-const merge = require('../../utils/merge-deep');
+const merge = require('../utils/merge-deep');
 
 // Return default configuration extended based on current environment
 module.exports = exports = merge(
