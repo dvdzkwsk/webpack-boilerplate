05ed640d08a8038ef4e8fc09041f868cce700d4e
David Zukowski <zukoweb@gmail.com>
2015-05-21 10:49:23 -0400

    refactor(karma): enables environment-specific karma configuration

--- build/karma/make.js
+++ build/karma/make.js
@@ -1,46 +1,7 @@
-const path = require('path');
-const webpack = require('webpack');
-const WEBPACK_CONFIG = require('../webpack/make')('development');
-const ENTRY = [
-  'app/index.js',
-  'app/spec.js'
-];
+import defaultConfig from './configs/default';
+import merge from '../utils/merge-deep';
 
-module.exports = config => {
-  config.set({
-    files : [
-      './node_modules/phantomjs-polyfill/bind-polyfill.js',
-      ...ENTRY
-    ],
-    frameworks : ['chai', 'mocha'],
-    preprocessors: ENTRY.reduce((memo, file) => {
-      memo[file] = ['webpack'];
-      return memo;
-    }, {}),
-    reporters: ['spec', 'coverage'],
-    coverageReporter: {
-      type : 'html',
-      dir  : 'dist/coverage/'
-    },
-    browsers: ['PhantomJS'],
-    singleRun : true,
-    webpack : {
-      resolve : WEBPACK_CONFIG.resolve,
-      module : {
-        loaders: WEBPACK_CONFIG.module.loaders
-      },
-      plugins : [WEBPACK_CONFIG.plugins[0]] // global variable definitions
-    },
-    webpackMiddleware : {
-      noInfo : true
-    },
-    plugins: [
-      require('karma-webpack'),
-      require('karma-mocha'),
-      require('karma-chai'),
-      require('karma-coverage'),
-      require('karma-phantomjs-launcher'),
-      require('karma-spec-reporter')
-    ]
-  });
-};
+export default config =>
+  karmaConfig => karmaConfig.set(
+    merge(defaultConfig, require(`./configs/${config.toLowerCase()}`))
+  );
--- karma.conf.js
+++ karma.conf.js
@@ -1,2 +1,4 @@
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 require('babel/register');
-module.exports = require('./build/karma/make');
+
+module.exports = require('./build/karma/make')(env);
