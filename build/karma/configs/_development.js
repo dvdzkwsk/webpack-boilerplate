333f2b1240ac3b56c2ad64493f2fedbf6b2f52b4
David Zukowski <zukoweb@gmail.com>
2015-07-21 18:00:16 -0400

    refactor(karma): environment-specific configurations now export functions to manipulate a provided config rather than being merged on top of a default config

--- build/karma/configs/_development.js
+++ build/karma/configs/_development.js
@@ -1,3 +1,5 @@
-export default {
-  singleRun : false
+export default (config) => {
+  config.singleRun = false;
+
+  return config;
 };
--- build/karma/make-config.js
+++ build/karma/make-config.js
@@ -1,11 +1,45 @@
-import assign from 'object-assign';
-import { NODE_ENV } from '../../config';
+import { NODE_ENV, SRC_DIRNAME } from '../../config';
+import makeWebpackConfig from '../webpack/make-config';
+
+const WEBPACK_CONFIG = makeWebpackConfig(
+  require('../webpack/client')
+);
+
+function makeDefaultConfig () {
+  return {
+    files : [
+      './node_modules/phantomjs-polyfill/bind-polyfill.js',
+      `${SRC_DIRNAME}/unit-test-entry.js`
+    ],
+    frameworks : ['chai', 'mocha'],
+    preprocessors : {
+      [`${SRC_DIRNAME}/**/*.js`] : ['webpack']
+    },
+    reporters : ['spec'],
+    browsers : ['PhantomJS'],
+    webpack : {
+      devtool : 'inline-source-map',
+      resolve : WEBPACK_CONFIG.resolve,
+      module  : {
+        loaders : WEBPACK_CONFIG.module.loaders
+      }
+    },
+    webpackMiddleware : {
+      noInfo : true
+    },
+    plugins: [
+      require('karma-webpack'),
+      require('karma-mocha'),
+      require('karma-chai'),
+      require('karma-coverage'),
+      require('karma-phantomjs-launcher'),
+      require('karma-spec-reporter')
+    ]
+  };
+}
 
 export default function (karmaConfig) {
   return karmaConfig.set(
-    assign(
-      require('./configs/default'),
-      require(`./configs/_${NODE_ENV}`)
-    )
+    require(`./configs/_${NODE_ENV}`)(makeDefaultConfig())
   );
 };
